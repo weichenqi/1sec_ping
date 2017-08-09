1dd7b0875f8b7c6bd8eff18b1c67654e59b853b5
GeorgeWei <weichenqi@gmail.com>
2017-08-09 10:34:08 +0800

    Initial commit

---
+++ 1sec_ping.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Time: 2017-08-09 09:00 
+# @Author  : George Wei (weichenqi@gmail.com)
+# @Link    : http://weichenqi.com
+# @Version : 0.1
+
+import pyping, datetime
+print datetime.datetime.now()
+for iplist in range(1, 255):
+    r = pyping.ping('192.168.1.'+str(iplist), count=1, packet_size=1, timeout=2, udp=True) #默认icmp ping，根据实际情况开启udp ping
+    print "192.168.1."+str(iplist) +" satus:" + str(r.ret_code)
+print datetime.datetime.now()
